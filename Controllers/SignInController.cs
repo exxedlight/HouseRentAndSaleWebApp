c2a6d317b91d82c8fe644efa8865b76531f581f3
Dann Ex <exxedlight@aol.com>
2024-05-07 00:35:17 +0300

    Добавьте файлы проекта.

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentAndSaleWebApp.Controllers
+{
+    public class SignInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRentAndSaleWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
