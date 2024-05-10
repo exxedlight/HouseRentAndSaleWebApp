8c056a772030473f9009d65a46bd1ea8f143094e
Dann Ex <exxedlight@aol.com>
2024-05-11 02:59:01 +0300

    Добавлена форма создания публикации, данные из нее уже идут на контроллер. Надо обрабатывать

---
+++ Controllers/PublicationController.cs
@@ -0,0 +1,25 @@
+﻿using HouseRentAndSaleWebApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentAndSaleWebApp.Controllers
+{
+    public class PublicationController : Controller
+    {
+        // GET: PublicationController
+        public ActionResult Index()
+        {
+            return View(new PublicationViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult Publish(PublicationViewModel model)
+        {
+
+            model.form_message = "Допиши бля!";
+
+            return View("Index", model);
+        }
+        
+    }
+}
---
+++ Models/PublicationViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace HouseRentAndSaleWebApp.Models
+{
+    public class PublicationViewModel
+    {
+        public string title { get; set; } = "";
+        public string adres { get; set; } = "";
+        public int operation_type { get; set; } = 1;
+        public int build_type { get; set; } = 1;
+        public string item_state { get; set; } = "new";
+        public int area { get; set; } = 1;
+        public string square { get; set; } = "";
+        public string floor { get; set; } = "";
+        public string price { get; set; } = "";
+        public string about { get; set; } = "";
+        public List<IFormFile> images { get; set; } = new List<IFormFile>();
+        public string? form_message { get; set; } = null;
+    }
+}
